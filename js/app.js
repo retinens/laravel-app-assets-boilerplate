814fc8799958cbdbd2b832cceb3c02702bed9108
WhereIsLucas <lucas@retinens.com>
2023-01-15 14:30:50 +0100

    wip

--- js/app.js
+++ js/app.js
@@ -1,11 +1,2 @@
-require('./bootstrap');
-
-const WOW = require('wowjs');
-window.wow = new WOW.WOW({
-    offset: 200,
-});
-window.wow.init();
-
-require("lozad");
-const observer = lozad();
-observer.observe();
+import * as bootstrap from 'bootstrap'
+window.bootstrap = bootstrap
